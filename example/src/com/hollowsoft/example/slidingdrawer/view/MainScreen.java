86a0cccfac2336f4e93263d5567e01f4918535e1
Igor Morais <mor41s.1gor@gmail.com>
2014-07-29 19:12:38 -0300

    Minor update

--- example/src/com/hollowsoft/example/slidingdrawer/view/MainScreen.java
+++ example/src/com/hollowsoft/example/slidingdrawer/view/MainScreen.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013 HollowSoft @Igor Morais
+ * Copyright (c) 2013 HollowSoft @IgorMorais
  *
  * Licensed under the Apache License, Version 2.0 (the “License”);
  * you may not use this file except in compliance with the License.
@@ -29,7 +29,7 @@ import com.hollowsoft.library.slidingdrawer.SlidingDrawer;
 
 /**
  * @author Igor Morais
- * @author Mor41s.1gor@gmail.com
+ * @author mor41s.1gor@gmail.com
  */
 public final class MainScreen extends Activity implements OnDrawerScrollListener, OnDrawerOpenListener,
 		OnDrawerCloseListener {
