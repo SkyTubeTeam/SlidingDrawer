c35bd0260db4b2ac046a8cbb395667a6fe6679d7
Igor Morais <mor41s.1gor@gmail.com>
2014-07-29 19:10:56 -0300

    Change header

--- code/src/com/hollowsoft/library/slidingdrawer/OnDrawerCloseListener.java
+++ code/src/com/hollowsoft/library/slidingdrawer/OnDrawerCloseListener.java
@@ -1,14 +1,14 @@
 /*
- * Copyright (C) 2008 The Android Open Source Project
+ * Copyright (c) 2014 HollowSoft @IgorMorais
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
+ * Licensed under the Apache License, Version 2.0 (the “License”);
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *          You may obtain a copy of the License at
+ *          http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
+ * distributed under the License is distributed on an “AS IS” BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
@@ -26,7 +26,7 @@ package com.hollowsoft.library.slidingdrawer;
  *      SlidingDrawer.OnDrawerCloseListener</a>
  *
  * @author Igor Morais
- * @author Mor41s.1gor@gmail.com
+ * @author mor41s.1gor@gmail.com
  */
 public interface OnDrawerCloseListener {
 
