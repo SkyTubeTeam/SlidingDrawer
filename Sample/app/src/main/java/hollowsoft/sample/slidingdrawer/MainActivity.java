fd14c14d50072d308ac29357848a70d56c80810b
Igor Morais <igor.morais@hollowsoft.com>
2017-09-13 21:03:02 -0300

    Minor update

--- Sample/app/src/main/java/hollowsoft/sample/slidingdrawer/MainActivity.java
+++ Sample/app/src/main/java/hollowsoft/sample/slidingdrawer/MainActivity.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2014 Igor Morais
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package hollowsoft.sample.slidingdrawer;
 
 import android.os.Bundle;
@@ -23,7 +39,7 @@ public final class MainActivity extends AppCompatActivity implements OnDrawerScr
 
         setContentView(R.layout.main);
 
-        final SlidingDrawer drawer = (SlidingDrawer) findViewById(R.id.drawer);
+        final SlidingDrawer drawer = findViewById(R.id.drawer);
 
         drawer.setOnDrawerScrollListener(this);
         drawer.setOnDrawerOpenListener(this);
